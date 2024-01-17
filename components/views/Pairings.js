a7286de5d5fb15df0661e776cea40aa3f7bc1072
Benjamin S <benjsheely@gmail.com>
2024-01-16 21:04:22 -0600

    Add Pairings and random name generator

--- components/views/Pairings.js
+++ components/views/Pairings.js
@@ -1,7 +1,21 @@
 import html from "html-literal";
 
-export default () => html`
-  <main>
-    <h1>View a list of potential pairings</h1>
+export default state => html`
+  <main class="pairings-page">
+    <div class="card-mini">
+      <h1>Potential Pairings</h1>
+      <div class="center-card">
+        <p>no pairings available</p>
+      </div>
+    </div>
+    <div class="card-mini">
+      <h1>Name Generator</h1>
+      <div class="center-card">
+        <button id="randomName" class="get-name">get name</button>
+        <div class="name-container">
+          <p>${state.randomName}</p>
+        </div>
+      </div>
+    </div>
   </main>
 `;
